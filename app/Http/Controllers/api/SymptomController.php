973656a32d134d6d0f856658042e4be3260d788b
Oldman2563 <oldm2563@gmail.com>
2026-03-16 13:59:41 +0000

    make the symptomcontroller with the create method

---
+++ app/Http/Controllers/api/SymptomController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\SymptomRequest;
+use App\Models\Symptom;
+use Illuminate\Http\Request;
+
+class SymptomController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create(SymptomRequest $request)
+    {
+        $incomingFields = $request->validated();
+        $incomingFields['user_id'] = auth()->id();
+        $symptom = Symptom::create($incomingFields);
+        return response()->json([
+            'success' => true,
+            'data' => $symptom,
+            'message' => 'a symptom has been added'
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
