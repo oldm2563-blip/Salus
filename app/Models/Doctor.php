bb01b4a8aaca506209e3510b63e06aa0df786504
Oldman2563 <oldm2563@gmail.com>
2026-03-18 12:28:48 +0000

    create Doctor model

---
+++ app/Models/Doctor.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Doctor extends Model
+{
+    protected $fillable = [
+        'name',
+        'specialty',
+        'city',
+        'yearsofexperience',
+        'consultation_price',
+        'available_days'
+    ];
+}
