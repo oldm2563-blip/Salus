6801cad90371db29b462f1b21f5bf8c05ac64562
Oldman2563 <oldm2563@gmail.com>
2026-03-26 09:52:02 +0100

    add the swagger setup

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,8 +1,25 @@
 <?php
 
 namespace App\Http\Controllers;
+use OpenApi\Attributes as OA;
+
+
+#[OA\Info(
+    title: 'medical API',
+    description: 'API for medical advices and finding doctors.',
+    version: '1.0.0',
+    )]
+    #[OA\SecurityScheme(
+        securityScheme: 'sanctum',
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+    )]
+    #[OA\Server(
+        url: '/api',
+        description: 'API Server',
+    )]
 
 abstract class Controller
 {
-    //
 }
